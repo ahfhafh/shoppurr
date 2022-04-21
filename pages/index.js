9d8f54c54561f710be6dba48b46fa9c191d54bf6
Jack <ahfhafh@gmail.com>
2022-04-21 17:35:48 -0600

    move navbar and background colour

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,15 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>ShopPurr</title>
         <meta name="description" content="Shop cat stuff" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className='bg-background'>
-        <Navbar />
+      
+      <main>
+        
       </main>
 
       <footer className='absolute text-center bottom-0 w-full'>
