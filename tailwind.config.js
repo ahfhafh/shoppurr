b483b21f497cbcda20e369b0f44e04c00158ceed
Jack <ahfhafh@gmail.com>
2022-04-21 17:59:39 -0600

    accent2

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
       'background': '#EFF0F3',
       'background2': '#FFFFFE',
       'text': '#0D0D0D',
-      'accent': '#ff8e3c'
+      'accent': '#ff8e3c',
+      'accent2': '#D9376E'
     },
     extend: {
       fontFamily: {
