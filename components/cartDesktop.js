0bb51e3e88f345690a47835fffb565f1ef7b1b46
Jack <ahfhafh@gmail.com>
2022-04-22 16:21:40 -0600

    little details

--- components/cartDesktop.js
+++ components/cartDesktop.js
@@ -1,6 +1,7 @@
 const CartDesktop = (props) => {
     return (
-        <div className="border-2 border-solid border-accent2 absolute right-0 h-full">
+        <div className="border-2 border-solid border-accent2 z-10 absolute right-0 h-full bg-background2">
+            <button className="float-right">X</button>
             <ul className="text-center">
                 {props.cartItems.map((item, i) => {
                     return (<li key={i} >{item.name}</li>)
--- components/cartMobile.js
+++ components/cartMobile.js
@@ -3,9 +3,10 @@
 const CartMobile = (props) => {
     return (
         <div className="border-2 border-solid border-accent2 z-10 absolute bottom-0 w-full bg-background2">
-            <ul className="text-center">
+            <button className="float-right">X</button>
+            <ul className="text-center py-2">
                 {props.cartItems.map((item, i) => {
-                    return (<li key={i} >{item.name}</li>)
+                    return (<li key={i} className='my-2' >{item.name} {item.price} {item.quanty}</li>)
                 })}
             </ul>
         </div>
