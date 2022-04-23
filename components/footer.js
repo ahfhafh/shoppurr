a54b6ce1d70c9fca37348f61881946a9893c87e8
Jack <ahfhafh@gmail.com>
2022-04-22 20:19:08 -0600

    move footer

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+const Footer = (props) => {
+    return (
+        <footer className='text-center bg-background2 pt-8 pb-36'>
+            Powered by{' '}Nothing
+        </footer>
+    );
+}
+
+export default Footer;
