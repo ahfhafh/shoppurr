fda4a28f9abe2867b9165989185a62ee690c325e
Jack <ahfhafh@gmail.com>
2022-04-22 22:54:58 -0600

    flex

--- components/cartDesktop.js
+++ components/cartDesktop.js
@@ -11,16 +11,16 @@ const CartDesktop = (props) => {
 
     return (
         <div className="bg-background2 bg-opacity-50 backdrop-blur-sm z-10 w-full h-full absolute" onClick={closeModal} ref={modalRef}>
-            <div className="bg-background2 shadow-2xl absolute pb-24 pt-20 right-0 w-[496px] ">
+            <div className="bg-background2 shadow-2xl absolute pt-20 right-0 w-[496px] h-full flex flex-col items-center">
                 <button className="absolute right-8 top-8" onClick={() => props.setToggleCart()}>X</button>
-                <ul className="text-center py-2 overflow-y-auto" style={{ 'maxHeight': 'calc(100vh - 205px)' }}>
+                <ul className="text-center py-2 overflow-y-auto grow w-full">
                     {props.cartItems.map((item, i) => {
                         return (<li key={i} className='my-2' >{item.name} {item.price} {item.quanty}</li>)
                     })}
                 </ul>
                 <p className="text-center mt-4">subtotal: </p>
                 <Link href='/'>
-                    <a className="bg-accent text-background2 absolute left-1/2 -translate-x-1/2 text-center text-xl w-10/12 mt-4 rounded-lg p-3" onClick={() => props.setToggleCart()}>Checkout</a>
+                    <a className="bg-accent text-background2 text-center text-xl w-10/12 mt-4 rounded-lg p-3 mb-8" onClick={() => props.setToggleCart()}>Checkout</a>
                 </Link>
             </div>
         </div>
