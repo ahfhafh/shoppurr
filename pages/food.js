de7028b815b2f520746b05bc39ecc64f007789bc
Jack <ahfhafh@gmail.com>
2022-04-29 23:13:26 -0600

    load food with image

--- pages/food.js
+++ pages/food.js
@@ -1,66 +1,39 @@
 import Image from 'next/image';
-import { useCollection } from 'react-firebase-hooks/firestore';
-import { useDownloadURL } from 'react-firebase-hooks/storage';
+import { useEffect, useState } from 'react';
 import firebase from '../firebase/app';
 
 const db = firebase.firestore();
-const storage = firebase.storage();
-
-var gsReference = storage.refFromURL('gs://shop-purr.appspot.com/food_1.jpg');
-
-// const docRef = doc(db, "Foods", "jqu976h5XkU3xdiJ6UDJ");
-// const docSnap = await getDoc(docRef);
-
-// if (docSnap.exists()) {
-//   console.log("Document data:", docSnap.data());
-// } else {
-//   // doc.data() will be undefined in this case
-//   console.log("No such document!");
-// }
-
-// var docRef = db.collection("Foods").doc("jqu976h5XkU3xdiJ6UDJ");
-
-// docRef.get().then((doc) => {
-//     if (doc.exists) {
-//         console.log("Document data:", doc.data());
-//     } else {
-//         // doc.data() will be undefined in this case
-//         console.log("No such document!");
-//     }
-// }).catch((error) => {
-//     console.log("Error getting document:", error);
-// });
 
 const Food = (props) => {
 
-    const [snapshot, loading, error] = useCollection(
-        db.collection("Foods"),
-        {}
-    );
+    const [products, setProducts] = useState([]);
 
-    const [value, loadingImg, errorImg] = useDownloadURL(gsReference);
+    useEffect(() => {
+        db.collection('Foods').get().then((snapshot) => {
+            setProducts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        });
+    }, [])
 
     return (
         <div>
-            {error && <strong>Error: {JSON.stringify(error)}</strong>}
-            {loading && <span>Collection: Loading...</span>}
-            {snapshot && (
-                <span>
-                    Collection:{' '}
-                    {snapshot.docs.map((doc) => (
-                        <div key={doc.id}>
-                            {value && <Image
-                                src={value}
-                                alt="Food 1"
-                                height="64px"
-                                width="64px" />}
-                            {JSON.stringify(doc.data())},{' '}
-                        </div>
-                    ))}
-                </span>
+            {/* {error && <strong>Error: {JSON.stringify(error)}</strong>}
+            {loading && <span>Loading...</span>} */}
+            {products && (console.log(products),
+                products.map((product) => (
+                    <div key={product.id}>
+                        <h2>{product.Name}</h2>
+                        <Image
+                            src={product.Image}
+                            alt="Food 1"
+                            height="256px"
+                            width="256px" />
+                    </div>
+                ))
             )}
-        </div >
+        </div>
     );
 }
 
 export default Food;
+
+// TODO: pagination
