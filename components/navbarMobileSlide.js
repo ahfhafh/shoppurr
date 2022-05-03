4a9e69a3c553c824fef8d05f84d6c5f27d1b4463
Jack <ahfhafh@gmail.com>
2022-05-02 18:27:08 -0600

    add some links

--- components/navbarMobileSlide.js
+++ components/navbarMobileSlide.js
@@ -6,14 +6,14 @@ const NavSlide = () => {
         <motion.div
             className="bg-accent w-10/12 left-1/2 -translate-x-1/2 h-0 fixed bottom-[95px]"
             key="navSlide" animate={{ height: "auto" }}
-            transition={{ duration: .2, type: "tween", ease: "easeOut" }}
+            transition={{ duration: 0.2, type: "tween", ease: "easeOut" }}
             exit={{ height: 0 }}
         >
             <motion.ul
                 className="text-background2 text-center py-2 opacity-0"
                 key="navSlideList"
                 animate={{ opacity: 1 }}
-                transition={{ duration: .1 }}
+                transition={{ duration: 0.1 }}
                 exit={{ opacity: 0 }}
             >
                 <li className="my-2">
@@ -29,21 +29,21 @@ const NavSlide = () => {
                     </Link>
                 </li>
                 <li className="my-2">
-                    <Link href='/'>
+                    <Link href='/food'>
                         <a className="text-xl">
                             Food
                         </a>
                     </Link>
                 </li>
                 <li className="my-2">
-                    <Link href='/'>
+                    <Link href='/toys'>
                         <a className="text-xl">
                             Toys
                         </a>
                     </Link>
                 </li>
                 <li className="my-2">
-                    <Link href='/'>
+                    <Link href='/clothing'>
                         <a className="text-xl">
                             Clothing
                         </a>
@@ -52,6 +52,8 @@ const NavSlide = () => {
             </motion.ul>
         </motion.div>
     );
-}
+};
 
-export default NavSlide
+export default NavSlide;
+
+// BUG: images appear above nav slide
