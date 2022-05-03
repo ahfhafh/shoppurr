77e7f730752c206d7836b28fc1827a988b6a6d1d
Jack <ahfhafh@gmail.com>
2022-05-02 21:33:02 -0600

    font preconnect

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=optional" rel="stylesheet"></link>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet" />
         </Head>
         <body className='bg-background'>
           <Main />
