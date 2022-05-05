d44f3da29cdbf2d6486d41a26cfb83be1f04dafd
Jack <ahfhafh@gmail.com>
2022-05-05 15:04:42 -0600

    BUG: hydration

--- components/navbarDesktopLinks.js
+++ components/navbarDesktopLinks.js
@@ -19,19 +19,17 @@ const NavbarDesktopLinks = (props) => {
             </div>
             <div className="ml-9 w-48 flex float-right justify-between">
 
-                <Link href='/account'>
-                    <a><FontAwesomeIcon icon={faUser} className="text-4xl text-accent" title="Profile" /></a>
-                </Link>
+                <Link href='/account'><a title="Profile">
+                    <FontAwesomeIcon icon={faUser} className="text-4xl text-accent" />
+                </a></Link>
 
-                <Link href='/'>
-                    <a>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} className="text-4xl text-accent" title="Search" />
-                    </a>
-                </Link>
+                <Link href='/'><a title="Search">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} className="text-4xl text-accent" />
+                </a></Link>
 
                 <button onClick={() => props.setToggleCart()}>
-                    <span className="fa-layers fa-fw fa-lg mt-2">
-                        <FontAwesomeIcon icon={faCartShopping} className="text-4xl text-accent" title="Cart" />
+                    <span className="fa-layers fa-fw fa-lg mt-2" title="Cart">
+                        <FontAwesomeIcon icon={faCartShopping} className="text-4xl text-accent" />
                         {props.cartItems > 0 && <span className="fa-layers-counter text-5xl" style={{ '--fa-counter-background-color': '#D9376E', '--fa-right': '-20px', '--fa-top': '-15px' }}>{props.cartItems}</span>}
                     </span>
                 </button>
