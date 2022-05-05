3e5a27e7ffb31b2bfe4d5e33583542579c91f1ae
Jack <ahfhafh@gmail.com>
2022-05-05 13:02:14 -0600

    unecessary div

--- components/cart.js
+++ components/cart.js
@@ -4,12 +4,12 @@ import { AnimatePresence } from "framer-motion";
 
 const Cart = (props) => {
 
-    return (<div>
+    return (
         <AnimatePresence exitBeforeEnter>{props.toggleCart && (props.useMediaQuery
             ? <CartMobile cartItems={props.cartItems} setToggleCart={() => props.setToggleCart()} removeCartItem={(i) => props.removeCartItem(i)} incNumInCart={(i) => props.incNumInCart(i)} decNumInCart={(i) => props.decNumInCart(i)} />
             : <CartDesktop cartItems={props.cartItems} setToggleCart={() => props.setToggleCart()} removeCartItem={(i) => props.removeCartItem(i)} incNumInCart={(i) => props.incNumInCart(i)} decNumInCart={(i) => props.decNumInCart(i)} />)
         }</AnimatePresence>
-    </div>);
+    );
 };
 
 export default Cart;
