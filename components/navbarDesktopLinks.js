6dee683b4dc6cd9227ff084aa8abf710f88289ba
Jack <ahfhafh@gmail.com>
2022-05-05 14:49:54 -0600

    mouseover

--- components/navbarDesktopLinks.js
+++ components/navbarDesktopLinks.js
@@ -20,18 +20,18 @@ const NavbarDesktopLinks = (props) => {
             <div className="ml-9 w-48 flex float-right justify-between">
 
                 <Link href='/account'>
-                    <a><FontAwesomeIcon icon={faUser} className="text-4xl text-accent" /></a>
+                    <a><FontAwesomeIcon icon={faUser} className="text-4xl text-accent" title="Profile" /></a>
                 </Link>
 
                 <Link href='/'>
                     <a>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} className="text-4xl text-accent" />
+                        <FontAwesomeIcon icon={faMagnifyingGlass} className="text-4xl text-accent" title="Search" />
                     </a>
                 </Link>
 
                 <button onClick={() => props.setToggleCart()}>
                     <span className="fa-layers fa-fw fa-lg mt-2">
-                        <FontAwesomeIcon icon={faCartShopping} className="text-4xl text-accent" />
+                        <FontAwesomeIcon icon={faCartShopping} className="text-4xl text-accent" title="Cart" />
                         {props.cartItems > 0 && <span className="fa-layers-counter text-5xl" style={{ '--fa-counter-background-color': '#D9376E', '--fa-right': '-20px', '--fa-top': '-15px' }}>{props.cartItems}</span>}
                     </span>
                 </button>
