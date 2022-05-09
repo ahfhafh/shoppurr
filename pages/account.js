f00333562605ca9d375026e255828e4e2503d24e
Jack <ahfhafh@gmail.com>
2022-05-08 20:09:08 -0600

    account final v1

--- pages/account.js
+++ pages/account.js
@@ -1,12 +1,12 @@
 import firebaseApp from "../firebase/app";
 import { useAuthState } from 'react-firebase-hooks/auth';
 import signout from "./api/logout";
-import { getAuth, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, fetchSignInMethodsForEmail } from "firebase/auth";
 import GoogleLogo from '../public/images/Google__G__Logo.svg';
 import FacebookLogo from '../public/images/Facebook_icon_2013.svg';
 import MailIcon from '../public/images/Email_icon.svg';
 import Loader from '/public/images/loader.svg';
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 const Account = () => {
@@ -22,16 +22,13 @@ const Account = () => {
             const token = credential.accessToken;
             // The signed-in user info.
             const user = result.user;
-            // ...
         }).catch((error) => {
             // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
+            console.log('errorCode: ' + error.code + ' errorMessage: ' + error.message);
             // The email of the user's account used.
             const email = error.email;
             // The AuthCredential type that was used.
             const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
         });
     }
 
@@ -43,18 +40,25 @@ const Account = () => {
             // This gives you a Facebook Access Token. You can use it to access the Facebook API.
             const credential = FacebookAuthProvider.credentialFromResult(result);
             const accessToken = credential.accessToken;
-
-            // ...
         }).catch((error) => {
             // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
+            console.log('errorCode: ' + error.code + ' errorMessage: ' + error.message);
             // The email of the user's account used.
             const email = error.email;
             // The AuthCredential type that was used.
             const credential = FacebookAuthProvider.credentialFromError(error);
+        });
+    }
 
-            // ...
+    function checkEmailExists(email) {
+        fetchSignInMethodsForEmail(auth, email).then((result) => {
+            if (result.length === 0) {
+                setUserExists(false);
+            } else {
+                setUserExists(true);
+            }
+        }).catch((error) => {
+            console.log(error);
         });
     }
 
@@ -62,11 +66,22 @@ const Account = () => {
         createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
             // Signed in
             const user = userCredential.user;
-            // ...
         }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ..
+            console.log('errorCode: ' + error.code + ' errorMessage: ' + error.message);
+            switch (error.code) {
+                case 'auth/email-already-exists':
+                    setEmailMessage('Email already exists');
+                    break;
+                case 'auth/user-not-found':
+                    setEmailMessage('User not found');
+                    break;
+                case 'auth/weak-password':
+                    setEmailMessage('Password too weak. Should be at least 6 characters long');
+                    break;
+                default:
+                    setEmailMessage('Errorrrrrr');
+                    break;
+            }
         });
     }
 
@@ -74,16 +89,59 @@ const Account = () => {
         signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
             // Signed in
             const user = userCredential.user;
-            // ...
         }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
+            console.log('errorCode: ' + error.code + ' errorMessage: ' + error.message);
+            switch (error.code) {
+                case 'auth/user-not-found':
+                    setEmailMessage('User not found');
+                    break;
+                case 'auth/wrong-password':
+                    setEmailMessage('Wrong password');
+                    break;
+                case 'auth/too-many-requests':
+                    setEmailMessage('Too many requests. Try again later.');
+                    break;
+                case 'auth/user-disabled':
+                    setEmailMessage('Ha uve been disabled. Get outta here');
+                    break;
+                default:
+                    setEmailMessage('Errorrrrrr');
+                    break;
+            }
         });
     }
 
     const [emailOpen, setEmailOpen] = useState(false);
+    const [userExists, setUserExists] = useState();
+    const [passwordStep, setPasswordStep] = useState(false);
+    const [emailMessage, setEmailMessage] = useState();
 
     const [user, loading, error] = useAuthState(auth);
+
+    function checkEmailHandler(e) {
+        e.preventDefault();
+        const email = e.target.elements.email.value;
+
+        if (passwordStep) {
+            const password = e.target.elements.password.value;
+            if (userExists) passwordAuthSignin(email, password);
+            else passwordAuthSignup(email, password);
+        } else {
+            checkEmailExists(email);
+        }
+        setPasswordStep(true);
+    }
+
+    useEffect(() => {
+        if (!passwordStep || !emailOpen) setEmailMessage();
+        if (emailOpen) emailInputRef.current.focus();
+    }, [passwordStep, emailOpen]);
+
+    useEffect(() => {
+        setEmailOpen(false);
+    }, [user]);
+
+    const emailInputRef = useRef();
 
     if (loading) {
         return (
@@ -106,34 +164,38 @@ const Account = () => {
                     {user.displayName}
                 </div>
 
-                : <motion.div className="px-4 py-4 max-w-xs mx-auto mt-[10vh] bg-background2" layout animate={{ height: 'auto' }}>
+                : <motion.div className="px-4 py-4 mb-16 max-w-xs mx-auto mt-[10vh] bg-background2" layout animate={{ height: 'auto' }}>
                     <motion.h1 className="text-center font-indie text-4xl mt-8 mb-24 block" layout>LOGIN</motion.h1>
-                        <div className='mb-8 flex flex-col items-center'>
-                            <motion.button className="w-72 py-2 px-4 border shadow-md relative h-11" onClick={googleAuth} layout>
-                                <GoogleLogo className='inline-block align-text-top absolute left-4' />
-                                <p className="text-base inline-block ml-8 mr-4">Continue with Google</p>
-                            </motion.button>
-                            <br />
-                            <motion.button className="w-72 py-2 px-4 border shadow-md relative bg-[#4267B2] h-11" onClick={FacebookAuth} layout>
-                                <FacebookLogo className='inline-block align-text-top absolute left-4' />
-                                <p className="text-base text-white inline-block ml-8 mr-4">Continue with Facebook</p>
-                            </motion.button>
-                            <br />
-                            <AnimatePresence>
-                                <motion.div className="w-72 border shadow-md relative bg-red-700" layout animate={{ height: 'auto' }}>
-                                    <motion.button layout className="w-full h-11" onClick={() => setEmailOpen(!emailOpen)}>
-                                        <MailIcon className='inline-block align-text-top absolute left-4 top-2' />
-                                        <p className="text-base text-white inline-block ml-8 mr-4 top-2">Continue with Email</p>
-                                    </motion.button>
-                                    {emailOpen &&
-                                        <motion.form className="mx-auto w-min my-4" layout initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
-                                            <input className="py-1 px-2 rounded-sm" type={'email'} placeholder={'Email'} required></input>
-                                            {/* <input className="py-1 px-2 rounded-sm" type={'password'} placeholder={'Password'} required></input> */}
-                                            <button type="submit" ></button>
-                                        </motion.form>}
-                                </motion.div>
-                            </AnimatePresence>
-                        </div>
+                    <div className='mb-8 flex flex-col items-center'>
+                        <motion.button className="w-72 py-2 px-4 border shadow-md relative h-11" onClick={googleAuth} layout>
+                            <GoogleLogo className='inline-block align-text-top absolute left-4' />
+                            <p className="text-base inline-block ml-8 mr-4">Continue with Google</p>
+                        </motion.button>
+                        <br />
+                        <motion.button className="w-72 py-2 px-4 border shadow-md relative bg-[#4267B2] h-11" onClick={FacebookAuth} layout>
+                            <FacebookLogo className='inline-block align-text-top absolute left-4' />
+                            <p className="text-base text-white inline-block ml-8 mr-4">Continue with Facebook</p>
+                        </motion.button>
+                        <br />
+                        <AnimatePresence>
+                            <motion.div className="w-72 border shadow-md relative bg-red-700" layout animate={{ height: 'auto' }}>
+                                <motion.button layout className="w-full h-11" onClick={() => { setEmailOpen(!emailOpen); }}>
+                                    <MailIcon className='inline-block align-text-top absolute left-4 top-2' />
+                                    <p className="text-base text-white inline-block ml-8 mr-4 top-2">Continue with Email</p>
+                                </motion.button>
+                                {emailOpen &&
+                                    <motion.form className="mx-auto w-min my-4" layout initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }} onSubmit={(e) => checkEmailHandler(e)}>
+                                        <motion.input className="py-1 px-2 my-1 rounded-sm" layout name="email" type={'email'} placeholder={'Email'} required ref={emailInputRef}></motion.input>
+                                        {passwordStep && <motion.input className="py-1 px-2 my-1 rounded-sm" layout name="password" type={'password'} placeholder={'Password'} required></motion.input>}
+                                        {(passwordStep && emailMessage) && <p className="text-lg font-semibold text-red-600 bg-red-300 rounded-sm px-2">{emailMessage}</p>}
+                                        <div>
+                                            {passwordStep && <motion.button className="mt-8 mb-4 mr-4 ml-4 py-2 text-white" layout onClick={() => setPasswordStep(false)}>cancel</motion.button>}
+                                            <motion.button className="mt-8 float-right mb-4 text-white bg-black rounded-sm py-2 px-4" layout type="submit">{(passwordStep) ? (userExists ? 'SIGN IN' : 'SIGN UP') : 'NEXT'}</motion.button>
+                                        </div>
+                                    </motion.form>}
+                            </motion.div>
+                        </AnimatePresence>
+                    </div>
                 </motion.div>
             }
         </>
